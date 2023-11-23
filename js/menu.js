1751717949c7d2d5a29bb542750cd53e8c8df356
Olexandr Lisovyi <microfox141120@outlook.com>
2023-11-24 00:17:27 +0200

    version 2 (add tabs and navigation between them)

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,13 @@
 const menuElements = document.querySelectorAll(".menu-item");
-menuElements.forEach((el) => {
+const calculatorContent = document.querySelector(".calculator__content");
+const contentWidth = calculatorContent.getBoundingClientRect().width;
+console.log(contentWidth);
+menuElements.forEach((el, i) => {
   el.addEventListener("click", function () {
     if (!el.classList.contains("active")) {
       document.querySelector(".menu-item.active").classList.remove("active");
       el.classList.add("active");
+      calculatorContent.style.marginLeft = -contentWidth * i + "px";
     }
   });
 });
